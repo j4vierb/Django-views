c41576e52f45723428fe017e94a93c8fbeebde80
Javier Barrera <js.barrerat1@uniandes.edu.co>
2024-08-22 21:52:28 -0500

    start from Django-models

---
+++ measurements/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from variables.models import Variable
+
+class Measurement(models.Model):
+  variable = models.ForeignKey(
+    Variable,
+    on_delete=models.CASCADE,
+    default=None
+  )
+  value = models.FloatField(null=True, blank=True, default=None)
+  unit = models.CharField(max_length=50)
+  place = models.CharField(max_length=50)
+  datetime = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+    return '{}: {}'.format(self.value, self.unit)
+  
